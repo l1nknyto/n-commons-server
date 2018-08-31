a56db066b18cfa2daef6c0479ab8a6150a37b349
Johny Jugianto <johny.jugianto@gmail.com>
2018-08-31 18:11:09 +0700

    Add forky

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
-function getMorganMW()
-{
+function getMorganMW() {
   const path = require('path');
   const morgan = require('morgan');
 
-  var logFile = path.join(process.env.LOG_PATH, process.env.APP_NAME.toLowerCase() +'-access-%DATE%.log');
+  var logFile = path.join(process.env.LOG_PATH, process.env.APP_NAME.toLowerCase() + '-access-%DATE%.log');
   var rotator = require('file-stream-rotator');
   var stream = rotator.getStream({
-    date_format : 'YYYYMMDD',
-    filename    : logFile,
-    frequency   : 'daily',
-    verbose     : false
+    date_format: 'YYYYMMDD',
+    filename: logFile,
+    frequency: 'daily',
+    verbose: false
   });
-  return morgan('combined', { stream:stream });
+  return morgan('combined', {
+    stream: stream
+  });
 }
 
 /**
@@ -22,6 +23,8 @@ function getMorganMW()
 module.exports = function(options) {
   const express = require('express');
   const session = require('express-session');
+  const toobusy = require('toobusy-js');
+  const logger  = require('n-commons/logger');
 
   var app = express();
   var server = null;
@@ -32,6 +35,13 @@ module.exports = function(options) {
   }
 
   //-- setup middleware
+  app.use(function(req, res, next) {
+    if (toobusy()) {
+      res.send(503, "Server busy right now.");
+    } else {
+      next();
+    }
+  });
   if (options.onSetupExpress) {
     options.onSetupExpress(app);
   }
@@ -41,7 +51,10 @@ module.exports = function(options) {
       app.use(item.url, express.static(item.folder));
     });
   }
-  app.use(require('body-parser').urlencoded({ extended: true }));
+
+  app.use(require('body-parser').urlencoded({
+    extended: true
+  }));
   app.use(require('cookie-parser')());
   app.use(session(options.sessionOptions));
   if (options.onFinishExpress) {
@@ -57,12 +70,22 @@ module.exports = function(options) {
     options.onSetupRouter(app);
   }
 
-  server.listen(options.port, function () {
-    console.log( 'Express server listening on port ' + server.address().port );
+  server.listen(options.port, function() {
+    logger.info('Express server listening on port ' + server.address().port);
+  });
+
+  process.on('uncaughtException', function(err) {
+    logger.error(err);
+    toobusy.shutdown();
+    try {
+      require('forky').disconnect();
+    } catch(e) {
+      console.error(e);
+    }
   });
 
   return {
-    app    : app,
-    server : server
+    app: app,
+    server: server
   };
 }
